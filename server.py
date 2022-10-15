bec971c245beea97fb9849603b603e570727aca7
Julian Lopez <a20490712@itmexicali.edu.mx>
2022-10-15 11:43:53 -0700

    Competency report

--- server.py
+++ server.py
@@ -48,6 +48,74 @@ def catalogue():
 @app.get("/api/products/count")
 def product_count():
     return json.dumps(len(catalog))
+
+# get /api/products/total
+# return the sum of all the prices
+@app.get("/api/products/total")
+def product_total():
+    total=0
+    for product in catalog:
+        total += product["price"]
+
+    return json.dumps(total) 
+
+# get /api/catalog/categoryName
+# return all the products that belong to  that category
+@app.get("/api/catalog/<category>")
+def by_category(category):
+    results = []
+    for prod in catalog:
+        if str.lower(prod["category"]) == str.lower(category):
+            results.append(prod)
+    
+    return json.dumps(results)
+
+# get /api/catalog/lower/<amount>
+# show those lower than amount
+@app.get("/api/catalog/lower/<amount>")
+def lower_then(amount):
+    results = []
+    for prod in catalog:
+        if prod["price"] < float(amount):
+            results.append(prod)
+    return json.dumps(results)
+
+# get /api/category/unique
+# get the list of unique categories
+@app.get("/api/category/unique")
+def categories_list():
+    categories = []
+    for prod in catalog:
+        category = prod["category"]
+        if not category in categories :
+            categories.append(category)
+    
+    return json.dumps(categories)
+
+@app.get("/api/test/colors")
+def unique_color():
+    colors = ["red", 'blue',"Pink", "yelloW", "Red", "Black", "BLUE", "RED", "BLACK", "YELLOW"]
+    results = []
+    for color in colors:
+        color = str.lower(color)
+        if color not in results:
+            results.append(color)
+    
+    return json.dumps(results)
+
+@app.get("/api/test/count/<color>")
+def count_color(color):
+    colors = ["red", 'blue',"Pink", "yelloW", "Red", "Black", "BLUE", "RED", "BLACK", "YELLOW"]
+
+    # parse your color to lower
+    color = color.lower()
+    # return the number of times the color appears in the list
+    counter = 0
+    for c in colors:
+        c = c.lower()
+        if color == c:
+            counter += 1
+    return json.dumps(counter)
 # run the app in debug mode
 app.run(debug=True)
 # When in production, turn off debug mode
